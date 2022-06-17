21ede100c470602c49a7a878ce71b740c6414450
Victor Gamayunov <vgamayunov@microsoft.com>
2022-06-17 12:51:30 +0100

    update run.sh example

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
 # Example how to run with correct thread pinning.
 
+NP=$(grep processor /proc/cpuinfo|wc -l)
+
 # ICPC + MKL
-OMP_NUM_THREADS=120 OMP_PLACES="{0}:120" ./dgemm-mkl-icpc
+OMP_NUM_THREADS=$NP OMP_PLACES="{0}:$NP" ./dgemm-mkl-icpc
 
 # GCC + BLIS
-OMP_NUM_THREADS=120 OMP_PLACES="{0}:120" ./dgemm-blis-gcc
-OMP_NUM_THREADS=120 OMP_PLACES=cores ./dgemm-blis-gcc
+OMP_NUM_THREADS=$NP OMP_PLACES="{0}:$NP" ./dgemm-blis-gcc
+OMP_NUM_THREADS=$NP OMP_PLACES=cores ./dgemm-blis-gcc
 
 # AOCC + BLIS
-OMP_NUM_THREADS=120 OMP_PLACES=nonsense ./dgemm-blis-aocc
+OMP_NUM_THREADS=$NP OMP_PLACES=nonsense ./dgemm-blis-aocc
 
 # ICPC + BLIS
-OMP_NUM_THREADS=120 KMP_AFFINITY=physical ./dgemm-blis-icpc
+OMP_NUM_THREADS=$NP KMP_AFFINITY=physical ./dgemm-blis-icpc
